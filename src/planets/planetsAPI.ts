cbec76679f614c58edb03305a408bcf8c564a084
crisdabro <criss.cele@gmail.com>
2023-05-05 14:46:52 +0200

    remove unused

--- src/planets/planetsAPI.ts
+++
@@ -1,8 +0,0 @@
-import { IPlanet } from "./types";
-
-export const fetchPlanet = async (id: number): Promise<IPlanet> => {
-  const res = await fetch(`https://swapi.dev/api/planets/${id}/`);
-  if (res.ok) {
-    return res.json();
-  } else throw new Error("Network response was not ok");
-};
